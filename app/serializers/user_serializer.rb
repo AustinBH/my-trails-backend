1ac697f4fb4b1f6563d9b2a13a31951f3961e82e
AustinBH <harlowaustin2@gmail.com>
2019-08-09 15:27:53 -0700

    updated user serializer to render only important data

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,22 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :likes, :comments, :completed_hikes
+
+  def likes
+    self.object.likes.map do |like|
+      {trail_id: like.trail_id,
+      user_id: like.user_id}
+    end
+  end
+  def comments
+    self.object.comments.map do |comment|
+      {trail_id: comment.trail_id,
+      user_id: comment.user_id}
+    end
+  end
+  def completed_hikes
+    self.object.completed_hikes.map do |completed_hike|
+      {trail_id: completed_hike.trail_id,
+      user_id: completed_hike.user_id}
+    end
+  end
 end
